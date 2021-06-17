956a82735fd88ff1f0ebcdc60469f8dabd46aad2
Suranjit Baroy <62200368+SuranjitBaroy@users.noreply.github.com>
2021-06-17 11:57:36 +0600

    Add files via upload
    Learn

---
+++ learn.js
@@ -0,0 +1,66 @@
+
+
+
+
+
+//JS Variable Declaration
+
+
+var data = "Data";
+
+let value = 20;
+
+const fixed = "Suranjit";
+
+console.log(data);
+console.log(value);
+console.log(fixed);
+
+
+
+//Singleline Comment
+
+/*Multipleline Comment*/
+
+
+
+
+
+/*JS Right way of variable decleration
+
+1. UPPERCASE
+2.Lowecase
+3.Casesensetive
+4.CamelCase
+
+*/
+
+
+var UPPERCASE     = "SURANJIT";
+
+var lowercase     = "lowercase";
+
+var casesensetive = "casesensetive";
+    casesensetive = "casesensetive or not";
+
+var CamelCase	  = "CamelCase";
+
+
+console.log(UPPERCASE);
+console.log(lowercase);
+console.log(casesensetive);
+console.log(CamelCase);
+
+
+/*
+
+Not usable method
+
+1. Space
+2. $ Sign 
+3. Hyphen
+4. Number Before
+
+*/
+
+
